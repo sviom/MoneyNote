6101b250f41ddbe045d287ef20f2cf7ba00a444e
HanByulKang <kanghanstar@outlook.com>
2020-01-12 23:20:07 +0900

    목록 보기 별도 컨트롤 분리 및 설정 추가 시작

---
+++ MoneyNoteUWP/UserControls/MoneyBasicList.xaml.cs
@@ -0,0 +1,86 @@
+﻿using MoneyNote.Views;
+using MoneyNoteLibrary.Models;
+using MoneyNoteLibrary.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// 사용자 정의 컨트롤 항목 템플릿에 대한 설명은 https://go.microsoft.com/fwlink/?LinkId=234236에 나와 있습니다.
+
+namespace MoneyNote.UserControls
+{
+    public sealed partial class MoneyBasicList : UserControl, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private MoneyViewModel _ViewModel;
+        public MoneyViewModel ViewModel
+        {
+            get { return _ViewModel; }
+            set
+            {
+                if (_ViewModel == value)
+                    return;
+
+                _ViewModel = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MoneyBasicList()
+        {
+            this.InitializeComponent();
+            this.Loaded += MoneyBasicList_Loaded;
+            this.Unloaded += MoneyBasicList_Unloaded;
+        }
+
+        private void MoneyBasicList_Loaded(object sender, RoutedEventArgs e)
+        {
+            ViewModel = new MoneyViewModel();
+        }
+
+        private void MoneyBasicList_Unloaded(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void MoneyListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (sender is ListView listView)
+            {
+                var selectedItem = listView.SelectedItem;
+                if (selectedItem is MoneyItem item)
+                {
+                    HomePage.CurrentHomePage.Frame.Navigate(typeof(MoneyDetailView), item);
+                }
+            }
+        }
+
+        private void NavigateCreatePage_Click(object sender, RoutedEventArgs e)
+        {
+            HomePage.CurrentHomePage.Frame.Navigate(typeof(MoneyCreateView));
+        }
+
+        private void LogOutButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
--- MoneyNoteUWP/Views/HomePage.xaml.cs
+++ MoneyNoteUWP/Views/HomePage.xaml.cs
@@ -34,19 +34,7 @@ namespace MoneyNote.Views
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
-        private MoneyViewModel _ViewModel;
-        public MoneyViewModel ViewModel
-        {
-            get { return _ViewModel; }
-            set
-            {
-                if (_ViewModel == value)
-                    return;
-
-                _ViewModel = value;
-                OnPropertyChanged();
-            }
-        }
+        public static HomePage CurrentHomePage;
 
         public HomePage()
         {
@@ -57,33 +45,11 @@ namespace MoneyNote.Views
 
         private void HomePage_Loaded(object sender, RoutedEventArgs e)
         {
-            ViewModel = new MoneyViewModel();
+            CurrentHomePage = this;
         }
 
         private void HomePage_Unloaded(object sender, RoutedEventArgs e)
         {
-        }
-
-        private void NavigateCreatePage_Click(object sender, RoutedEventArgs e)
-        {
-            Frame.Navigate(typeof(MoneyCreateView));
-        }
-
-        private void MoneyListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            if (sender is ListView listView)
-            {
-                var selectedItem = listView.SelectedItem;
-                if (selectedItem is MoneyItem item)
-                {
-                    this.Frame.Navigate(typeof(MoneyDetailView), item);
-                }
-            }
-        }
-
-        private void LogOutButton_Click(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
