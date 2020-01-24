7930ec04d435daca258919f85d37efbeefbeb0f5
HanByulKang <kanghanstar@outlook.com>
2020-01-24 19:11:06 +0900

    카테고리 뷰에서 추가 로직 수정중

---
+++ MoneyNoteLibrary/ViewModels/MainCategoryViewModel.cs
@@ -0,0 +1,156 @@
+﻿using MoneyNoteLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using static MoneyNoteLibrary.Enums.MoneyEnum;
+
+namespace MoneyNoteLibrary.ViewModels
+{
+    public class MainCategoryViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private MoneyCategory _Division;
+        public MoneyCategory Division
+        {
+            get { return _Division; }
+            set
+            {
+                if (_Division == value)
+                    return;
+
+                _Division = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _SaveButtonText = "저장";
+        public string SaveButtonText
+        {
+            get { return _SaveButtonText; }
+            set
+            {
+                if (_SaveButtonText == value)
+                    return;
+
+                _SaveButtonText = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool _IsShowSubCategory;
+        public bool IsShowSubCategory
+        {
+            get { return _IsShowSubCategory; }
+            set
+            {
+                if (_IsShowSubCategory == value)
+                    return;
+
+                _IsShowSubCategory = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(SaveButtonText));
+            }
+        }
+
+        private bool _IsShowAddNewCategory;
+        public bool IsShowAddNewCategory
+        {
+            get { return _IsShowAddNewCategory; }
+            set
+            {
+                if (_IsShowAddNewCategory == value)
+                    return;
+
+                _IsShowAddNewCategory = value;
+
+                if (value)
+                    SaveButtonText = "새로운 카테고리 추가";
+                else
+                    SaveButtonText = "저장";
+
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(SaveButtonText));
+            }
+        }
+
+        private string _CategoryText;
+        public string CategoryText
+        {
+            get { return _CategoryText; }
+            set
+            {
+                if (_CategoryText == value)
+                    return;
+
+                _CategoryText = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private List<MainCategory> _MainCategories;
+        public List<MainCategory> MainCategories
+        {
+            get { return _MainCategories; }
+            set
+            {
+                if (_MainCategories == value)
+                    return;
+
+                _MainCategories = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private List<SubCategory> _SubCategories;
+        public List<SubCategory> SubCategories
+        {
+            get { return _SubCategories; }
+            set
+            {
+                if (_SubCategories == value)
+                    return;
+
+                _SubCategories = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainCategoryViewModel(MoneyCategory div)
+        {
+            Division = div;
+        }
+
+        public void Intialize()
+        {
+            // 수입 지출 구분에 따른 해당 메인 카테고리들 가져오기
+        }
+
+        public void SaveCategory()
+        {
+            var category = new MainCategory()
+            {
+                Title = CategoryText
+            };
+
+            // 저장
+        }
+
+        public void SetSaveButton()
+        {
+            if (IsShowSubCategory)
+            {
+            }
+            else if (IsShowAddNewCategory)
+            {
+            }
+        }
+    }
+}
--- MoneyNoteUWP/UserControls/MainCategorySetting.xaml.cs
+++ MoneyNoteUWP/UserControls/MainCategorySetting.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using MoneyNoteLibrary.ViewModels;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -12,16 +15,69 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using static MoneyNoteLibrary.Enums.MoneyEnum;
 
 // 사용자 정의 컨트롤 항목 템플릿에 대한 설명은 https://go.microsoft.com/fwlink/?LinkId=234236에 나와 있습니다.
 
 namespace MoneyNote.UserControls
 {
-    public sealed partial class MainCategorySetting : UserControl
+    public sealed partial class MainCategorySetting : UserControl, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private MainCategoryViewModel _ViewModel;
+        public MainCategoryViewModel ViewModel
+        {
+            get { return _ViewModel; }
+            set
+            {
+                if (_ViewModel == value)
+                    return;
+
+                _ViewModel = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public static readonly DependencyProperty DivisionProperty =
+            DependencyProperty.Register("Division", typeof(MoneyCategory), typeof(MainCategorySetting), new PropertyMetadata(default(MoneyCategory)));
+
+        public MoneyCategory Division
+        {
+            get { return (MoneyCategory)GetValue(DivisionProperty); }
+            set { SetValue(DivisionProperty, value); }
+        }
+
         public MainCategorySetting()
         {
             this.InitializeComponent();
+            this.Loaded += MainCategorySetting_Loaded;
+            this.Unloaded += MainCategorySetting_Unloaded;
+        }
+
+        private void MainCategorySetting_Loaded(object sender, RoutedEventArgs e)
+        {
+            ViewModel = new MainCategoryViewModel(Division);
+        }
+
+        private void MainCategorySetting_Unloaded(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void MainCategoryListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (MainCategoryListView.SelectedItem != null)
+                ViewModel.IsShowSubCategory = true;
+        }
+
+        private void SaveCategory_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.SaveCategory();
         }
     }
 }
