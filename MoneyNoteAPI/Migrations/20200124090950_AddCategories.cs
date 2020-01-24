109694f73479f772d11a01225fdd3713da50c4cf
HanByulKang <kanghanstar@outlook.com>
2020-01-24 18:10:13 +0900

    마이그레이션 및 디비 업데이트

---
+++ MoneyNoteAPI/Migrations/20200124090950_AddCategories.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MoneyNoteAPI.Migrations
+{
+    public partial class AddCategories : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MainCategory",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Title = table.Column<string>(nullable: false),
+                    Division = table.Column<int>(nullable: false),
+                    CreatedTime = table.Column<DateTimeOffset>(nullable: false),
+                    UpdatedTime = table.Column<DateTimeOffset>(nullable: false),
+                    UserId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MainCategory", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MainCategory_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SubCategory",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Title = table.Column<string>(nullable: false),
+                    Division = table.Column<int>(nullable: false),
+                    CreatedTime = table.Column<DateTimeOffset>(nullable: false),
+                    UpdatedTime = table.Column<DateTimeOffset>(nullable: false),
+                    MainCategoryId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SubCategory", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SubCategory_MainCategory_MainCategoryId",
+                        column: x => x.MainCategoryId,
+                        principalTable: "MainCategory",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MainCategory_UserId",
+                table: "MainCategory",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SubCategory_MainCategoryId",
+                table: "SubCategory",
+                column: "MainCategoryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SubCategory");
+
+            migrationBuilder.DropTable(
+                name: "MainCategory");
+        }
+    }
+}
