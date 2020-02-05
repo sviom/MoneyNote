a3b7ccc8f5493e6bd54017787d22654ef8b4327e
HanByulKang <kanghanstar@outlook.com>
2020-02-05 21:33:27 +0900

    데이터 정리 및 마이그레이션 재정리

---
+++ MoneyNoteAPI/Migrations/20200205122243_InitialMigration.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MoneyNoteAPI.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: false),
+                    Password = table.Column<string>(nullable: false),
+                    ConfirmPassword = table.Column<string>(nullable: true),
+                    CreatedTime = table.Column<DateTimeOffset>(nullable: false),
+                    UpdatedTime = table.Column<DateTimeOffset>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MainCategories",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Title = table.Column<string>(nullable: false),
+                    Division = table.Column<int>(nullable: false),
+                    CreatedTime = table.Column<DateTimeOffset>(nullable: false),
+                    UpdatedTime = table.Column<DateTimeOffset>(nullable: false),
+                    UserId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MainCategories", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MainCategories_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SubCategories",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Title = table.Column<string>(nullable: false),
+                    Division = table.Column<int>(nullable: false),
+                    CreatedTime = table.Column<DateTimeOffset>(nullable: false),
+                    UpdatedTime = table.Column<DateTimeOffset>(nullable: false),
+                    MainCategoryId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SubCategories", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SubCategories_MainCategories_MainCategoryId",
+                        column: x => x.MainCategoryId,
+                        principalTable: "MainCategories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MoneyItems",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Title = table.Column<string>(nullable: false),
+                    Money = table.Column<double>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    Division = table.Column<int>(nullable: false),
+                    CreatedTime = table.Column<DateTimeOffset>(nullable: false),
+                    UpdatedTime = table.Column<DateTimeOffset>(nullable: false),
+                    UserId = table.Column<Guid>(nullable: false),
+                    MainCategoryId = table.Column<Guid>(nullable: false),
+                    SubCategoryId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MoneyItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MoneyItems_MainCategories_MainCategoryId",
+                        column: x => x.MainCategoryId,
+                        principalTable: "MainCategories",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_MoneyItems_SubCategories_SubCategoryId",
+                        column: x => x.SubCategoryId,
+                        principalTable: "SubCategories",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_MoneyItems_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MainCategories_UserId",
+                table: "MainCategories",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MoneyItems_MainCategoryId",
+                table: "MoneyItems",
+                column: "MainCategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MoneyItems_SubCategoryId",
+                table: "MoneyItems",
+                column: "SubCategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MoneyItems_UserId",
+                table: "MoneyItems",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SubCategories_MainCategoryId",
+                table: "SubCategories",
+                column: "MainCategoryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MoneyItems");
+
+            migrationBuilder.DropTable(
+                name: "SubCategories");
+
+            migrationBuilder.DropTable(
+                name: "MainCategories");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
