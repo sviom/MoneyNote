dc896f4ad26281066205e73117a076d80fe1f81c
강한별 <kanghanstar@outlook.com>
2020-06-24 22:46:40 +0900

    새로운 프로젝트 포팅

---
+++ MoneyNoteNew/Converter/XamlConverter.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+using static MoneyNoteLibrary.Enums.MoneyEnum;
+
+namespace MoneyNoteNew.Converter
+{
+    public class DivisionColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var color = new SolidColorBrush(Colors.Black);
+            if (value is MoneyCategory category)
+            {
+                switch (category)
+                {
+                    case MoneyCategory.Expense:
+                        color = new SolidColorBrush(Colors.Red);
+                        break;
+                    case MoneyCategory.Income:
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return color;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return new SolidColorBrush(Colors.Black);
+        }
+    }
+
+    public class MoneyDividerConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            string result = string.Empty;
+            if (value is double money)
+            {
+                result = money.ToString("C0");
+            }
+
+            return result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return value.ToString();
+        }
+    }
+
+    public class DateTimeToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            string result = string.Empty;
+            if (value is DateTimeOffset dateTime)
+            {
+                result = dateTime.ToString("yyyy-MM-dd");
+            }
+            return result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return "";
+        }
+    }
+
+    public class NullToBoolean : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value == null)
+                return true;
+
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return false;
+        }
+    }
+
+    public class NotNullToBoolean : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value != null)
+                return true;
+
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return false;
+        }
+    }
+
+
+
+    #region Visibility 관련
+
+
+    public class StringNotNullToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var result = Visibility.Collapsed;
+            if (value is string item)
+            {
+                if (!string.IsNullOrEmpty(item))
+                    result = Visibility.Visible;
+            }
+            return result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return Visibility.Collapsed;
+        }
+    }
+
+    public class NullToVisibility : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var result = Visibility.Collapsed;
+            if (value == null)
+                result = Visibility.Visible;
+            return result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return Visibility.Collapsed;
+        }
+    }
+
+    public class NotNullToVisibility : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var result = Visibility.Collapsed;
+            if (value != null)
+                result = Visibility.Visible;
+            return result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return Visibility.Collapsed;
+        }
+    }
+
+    public class CountVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is int count)
+            {
+                if (count > 0)
+                    return Visibility.Visible;
+            }
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return Visibility.Collapsed;
+        }
+    }
+
+    public class CountVisibilityConverterReverse : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is int count)
+            {
+                if (count <= 0)
+                    return Visibility.Visible;
+            }
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return Visibility.Collapsed;
+        }
+    }
+
+    #endregion
+}
---
+++ MoneyNoteNew/MainPage.xaml.cs
@@ -0,0 +1,139 @@
+﻿using MoneyNoteLibrary.ViewModels;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// 빈 페이지 항목 템플릿에 대한 설명은 https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x412에 나와 있습니다.
+
+namespace MoneyNoteNew
+{
+    /// <summary>
+    /// 자체적으로 사용하거나 프레임 내에서 탐색할 수 있는 빈 페이지입니다.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private LoginViewModel _ViewModel;
+        public LoginViewModel ViewModel
+        {
+            get { return _ViewModel; }
+            set
+            {
+                if (_ViewModel == value)
+                    return;
+
+                _ViewModel = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.Loaded += MainPage_Loaded;
+            this.Unloaded += MainPage_Unloaded;
+        }
+
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            ViewModel = new LoginViewModel();
+            GetSavedId();
+        }
+
+        private void MainPage_Unloaded(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private async void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            await Login();
+        }
+
+        private void SignUpButton_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(Views.SignUpPage));
+        }
+
+        private async void PasswordTextBox_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == Windows.System.VirtualKey.Enter)
+            {
+                await Login();
+            }
+        }
+
+        public async Task Login()
+        {
+            (var result, var user) = await ViewModel.LogIn();
+            if (result)
+            {
+                if (ViewModel.IsIdSaveChecked)
+                    await SaveId();
+                //AzureKeyVault.SaltPassword = await AzureKeyVault.OnGetAsync(KeyVaultName.SaltPassword.ToString());
+                App.LogInedUser = user;
+                Frame.Navigate(typeof(Views.HomePage));
+            }
+        }
+
+        private async Task SaveId()
+        {
+            var idText = IdTextBox.Text;
+            if (string.IsNullOrEmpty(idText))
+                return;
+
+            var storageFolder = ApplicationData.Current.LocalFolder;
+            var sampleFile = await storageFolder.CreateFileAsync(LoginViewModel.SavedIdTextFile, CreationCollisionOption.ReplaceExisting);
+
+            var saveForm = new SaveIdForm();
+            saveForm.Id = idText;
+            saveForm.IsSaveChecked = ViewModel.IsIdSaveChecked;
+            var jsonText = JsonConvert.SerializeObject(saveForm);
+
+            await FileIO.WriteTextAsync(sampleFile, jsonText);
+        }
+
+        public async void GetSavedId()
+        {
+            var storageFolder = ApplicationData.Current.LocalFolder;
+            var isFileExist = await storageFolder.TryGetItemAsync(LoginViewModel.SavedIdTextFile);
+
+            if (isFileExist == null)
+                return;
+
+            var sampleFile = await storageFolder.GetFileAsync(LoginViewModel.SavedIdTextFile);
+            if (sampleFile != null)
+            {
+                string text = await FileIO.ReadTextAsync(sampleFile);
+                var saveIdForm = JsonConvert.DeserializeObject<SaveIdForm>(text);
+                if (saveIdForm.IsSaveChecked)
+                {
+                    IdTextBox.Text = saveIdForm.Id;
+                    if (ViewModel != null) ViewModel.IsIdSaveChecked = saveIdForm.IsSaveChecked;
+                }
+            }
+
+        }
+    }
+}
