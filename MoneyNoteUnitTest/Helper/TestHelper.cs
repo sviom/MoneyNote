774580495a8d69fd585d2d86d3827a5fa8ca247a
강한별 <kanghanstar@outlook.com>
2020-07-18 14:25:56 +0900

    테스트헬퍼 및 금액 관련 테스트 제작 시작

--- MoneyNoteUnitTest/Helper/TestHelper.cs
+++ MoneyNoteUnitTest/Helper/TestHelper.cs
@@ -26,5 +26,48 @@ namespace MoneyNoteUnitTest.Helper
             user = userService.SignUp(newUser);
             return user;
         }
+
+        public static BankBook CreateBankBook(MoneyContext context, User user)
+        {
+            var service = new BankBookService(context);
+
+            var testTitle = Guid.NewGuid().ToString();
+            var newItem = new BankBook
+            {
+                Name = testTitle,
+                User = user,
+                UserId = user.Id
+            };
+
+            var result = service.SaveBankBook(newItem);
+
+            return result;
+        }
+
+        public static MainCategory CreateCategory(MoneyContext context, User user)
+        {
+            var testTitle = Guid.NewGuid().ToString();
+            var service = new CategoryService(context);
+            var newCategory = new MainCategory();
+            newCategory.User = user;
+            newCategory.UserId = user.Id;
+            newCategory.Title = testTitle;
+
+            var saveResult = service.SaveCategory(newCategory);
+
+            return (MainCategory)saveResult;
+        }
+
+        public static (User user, BankBook bankbook, MainCategory category) CreateSeed(MoneyContext context)
+        {
+            // user
+            var user = CreateTestAccount(context);
+            // bankbook
+            var bankbook = CreateBankBook(context, user);
+            // category
+            var category = CreateCategory(context, user);
+
+            return (user, bankbook, category);
+        }
     }
 }
--- MoneyNoteUnitTest/ServiceTest/MoneyServiceTest.cs
+++ MoneyNoteUnitTest/ServiceTest/MoneyServiceTest.cs
@@ -1,8 +1,10 @@
-﻿using MoneyNoteAPI.Services;
+﻿using Microsoft.EntityFrameworkCore.Internal;
+using MoneyNoteAPI.Services;
 using MoneyNoteLibrary.Models;
 using MoneyNoteUnitTest.Helper;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Xunit;
 
@@ -15,21 +17,31 @@ namespace MoneyNoteUnitTest.ServiceTest
         public SharedDatabaseFixture Fixture { get; }
 
         [Fact]
-        public void Test()
+        public void SaveMoney()
         {
             using var context = Fixture.CreateContext();
-            var testAccount = TestHelper.CreateTestAccount(context);
+            (var testAccount, var bankbook, var category) = TestHelper.CreateSeed(context);
             var service = new MoneyService(context);
 
+            var testMoney = 100000;
+            var testTitle = Guid.NewGuid().ToString();
             var newItem = new MoneyItem();
-
+            newItem.Money = testMoney;
+            newItem.Title = testTitle;
             newItem.UserId = testAccount.Id;
+            newItem.User = testAccount;
+            newItem.MainCategory = category;
+            newItem.BankBook = bankbook;
 
             var savedItem = service.SaveMoney(newItem);
 
             var resultItem = service.GetMoney(x => x.Id == savedItem.Id);
+            var resultBank = context.BankBooks.Where(x => x.Id == bankbook.Id).FirstOrDefault();
 
             Assert.NotNull(resultItem);
+            Assert.Equal(testMoney, resultItem.Money);
+            Assert.Equal(testTitle, resultItem.Title);
+            Assert.Equal(-testMoney, resultBank.Assets);
         }
     }
 }
