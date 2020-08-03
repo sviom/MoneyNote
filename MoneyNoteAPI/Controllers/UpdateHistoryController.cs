2e1fb3360efcfa2fbbc9c70b2c5dbb2a24459585
강한별 <kanghanstar@outlook.com>
2020-08-03 23:14:06 +0900

    히스토리 내역서비스 추가 시작

---
+++ MoneyNoteAPI/Controllers/UpdateHistoryController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MoneyNoteLibrary;
+using MoneyNoteLibrary.Models;
+
+namespace MoneyNoteAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UpdateHistoryController : ControllerBase
+    {
+        public List<UpdateHistory> GetBankBooks(UpdateHistory user)
+        {
+            try
+            {
+                using var context = new MoneyContext();
+                return context.BankBooks.Where(x => x.UserId == user.Id).ToList();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+    }
+}
--- MoneyNoteLibrary/Models/UpdateHistory.cs
+++ MoneyNoteLibrary/Models/UpdateHistory.cs
@@ -13,6 +13,9 @@ namespace MoneyNoteLibrary.Models
         public Guid Id { get; set; }
 
         [Required]
+        public string Title { get; set; }
+
+        [Required]
         public string Content { get; set; }
 
         [Required]
