0d6810f71c6382bf9af6333a31e8d5cdeed06f37
강한별 <kanghanstar@outlook.com>
2020-12-30 13:54:27 +0900

    keyvault 누겟 변경에 따른 구성 변경

--- MoneyNoteLibrary/Common/AzureKeyVault.cs
+++ MoneyNoteLibrary/Common/AzureKeyVault.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.Azure.KeyVault;
-using Microsoft.Azure.KeyVault.Models;
+﻿//using Microsoft.Azure.KeyVault;
+//using Microsoft.Azure.KeyVault.Models;
+using Azure.Core;
+using Azure.Identity;
+using Azure.Security.KeyVault;
+using Azure.Security.KeyVault.Secrets;
 using Microsoft.Azure.Services.AppAuthentication;
 using System;
 using System.Collections.Generic;
@@ -12,24 +16,41 @@ namespace MoneyNoteLibrary.Common
     {
         public static string SaltPassword => OnGetAsync("SaltPassword").Result;
 
-        public static string GetKeyVaultEndpoint() => "https://todaylunchkeyvault.vault.azure.net";
+        public static string GetKeyVaultEndpoint() => "https://todaylunchkeyvault.vault.azure.net/";
 
         public static async Task<string> OnGetAsync(string secretName)
         {
-            var secretValue = "Your application description page.";
+            string secretValue;
             try
             {
-                var azureServiceTokenProvider = new AzureServiceTokenProvider();
-                var keyVaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
-                var secret = await keyVaultClient.GetSecretAsync(GetKeyVaultEndpoint() + "/secrets/" + secretName).ConfigureAwait(false);
-                secretValue = secret.Value;
+                //var azureServiceTokenProvider = new AzureServiceTokenProvider();
+                //var keyVaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+                //var secret = await keyVaultClient.GetSecretAsync(GetKeyVaultEndpoint() + "/secrets/" + secretName).ConfigureAwait(false);
+                //secretValue = secret.Value;
+
+                var options = new SecretClientOptions()
+                {
+                    Retry =
+                    {
+                        Delay= TimeSpan.FromSeconds(2),
+                        MaxDelay = TimeSpan.FromSeconds(16),
+                        MaxRetries = 5,
+                        Mode = RetryMode.Exponential
+                    }
+                };
+                var client = new SecretClient(new Uri(GetKeyVaultEndpoint()), new DefaultAzureCredential(), options);
+
+                var secret = await client.GetSecretAsync(secretName);
+                secretValue = secret.Value.Value;
             }
-            catch (KeyVaultErrorException keyVaultException)
+            //catch (KeyVaultErrorException keyVaultException)
+            catch (Exception ex)
             {
-                secretValue = keyVaultException.Message;
+                secretValue = ex.Message;
             }
             return secretValue;
         }
+
     }
 
     public enum KeyVaultName
