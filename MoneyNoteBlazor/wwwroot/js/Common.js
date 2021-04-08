e763711c4ec7776be126f554768fd266d7dfdb8f
강한별 <kanghanstar@outlook.com>
2021-04-08 22:04:17 +0900

    로컬 스토리지에도 사용자 정보 저장하도록 추가

---
+++ MoneyNoteBlazor/wwwroot/js/Common.js
@@ -0,0 +1,13 @@
+﻿window.SetUserInfo = (user) => {
+    console.log("Set Userinfo", user);
+    const localStorage = window.localStorage;
+    localStorage.setItem("userKey", user);
+
+    return true;
+}
+
+window.GetUserInfo = (key) => {
+    console.log("Get Userinfo", key);
+    const localStorage = window.localStorage;
+    return localStorage.getItem(key);
+}
