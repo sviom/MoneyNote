94d49c8bb758cbbd465cc6cde84e776c188ba576
강한별 <kanghanstar@outlook.com>
2021-07-15 21:51:16 +0900

    기존 마이그레이션 복사

---
+++ MoneyNoteLibrary5/Migrations/20200226150248_BankBookColumnAdd_Assets.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MoneyNoteLibrary5.Migrations
+{
+    public partial class BankBookColumnAdd_Assets : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Assets",
+                table: "BankBooks",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "BankBooks",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Assets",
+                table: "BankBooks");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "BankBooks");
+        }
+    }
+}
