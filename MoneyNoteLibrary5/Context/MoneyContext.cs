d450e100f4c95e4627f08ff771c149054bb1f984
강한별 <kanghanstar@outlook.com>
2021-07-15 21:43:54 +0900

    클래스 라이브러리에서 EFCore 마이그레이션

---
+++ MoneyNoteLibrary5/Context/MoneyContext.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+//using MoneyNoteLibrary.Common;
+using MoneyNoteLibrary5.Models;
+using MoneyNoteLibrary5.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoneyNoteLibrary5.Context
+{
+    public class MoneyContext : DbContext
+    {
+        public MoneyContext() { }
+
+        public MoneyContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<MoneyItem> MoneyItems { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<MainCategory> MainCategories { get; set; }
+
+        public DbSet<SubCategory> SubCategories { get; set; }
+
+        public DbSet<BankBook> BankBooks { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //base.OnConfiguring(optionsBuilder);
+            if (!optionsBuilder.IsConfigured)
+            {
+                //optionsBuilder.UseSqlServer(AzureKeyVault.OnGetAsync(KeyVaultName.MoneyNoteConnectionString).Result);
+                optionsBuilder.UseSqlServer(AzureKeyVault.OnGetAsync(KeyVaultName.MoneyNoteConnectionString).Result, x=>x.MigrationsAssembly("MoneyNoteLibrary5"));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+             .HasMany(x => x.MoneyItems)
+             .WithOne(y => y.User)
+             .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<User>()
+                .HasMany(x => x.MainCategories)
+                .WithOne(y => y.User)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<User>()
+              .HasMany(x => x.BankBooks)
+              .WithOne(y => y.User)
+              .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<MoneyItem>()
+                .HasOne(x => x.MainCategory)
+                .WithMany()
+                .OnDelete(DeleteBehavior.Restrict);
+
+            //modelBuilder.Entity<MoneyItem>()
+            //    .HasOne(x => x.User)
+            //    .WithMany()
+            //    .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<MoneyItem>()
+                .HasOne(x => x.BankBook)
+                .WithMany()
+                .OnDelete(DeleteBehavior.NoAction);
+
+            //modelBuilder.Entity<MainCategory>()
+            //    .HasOne(x => x.User)
+            //    .WithMany(y => y.MainCategories)
+            //    .OnDelete(DeleteBehavior.NoAction);
+
+            //modelBuilder.Entity<BankBook>()
+            //    .HasMany(x => x.MoneyItems)
+            //    .WithOne(y => y.BankBook)
+            //    .OnDelete(DeleteBehavior.NoAction);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ MoneyNoteLibrary5/Context/SqlLauncher.cs
@@ -0,0 +1,183 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoneyNoteLibrary5;
+using MoneyNoteLibrary5.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace MoneyNoteLibrary5.Context
+{
+    public class SqlLauncher
+    {
+        public static T Insert<T>(T inputObject) where T : class
+        {
+            T addedObject = null;
+            try
+            {
+                using var db = new MoneyContext();
+                db.Entry(inputObject).State = Microsoft.EntityFrameworkCore.EntityState.Added;
+                var set = db.Set<T>();
+                set.Add(inputObject);
+                int saveResult = db.SaveChanges();
+                if (saveResult > 0)
+                    addedObject = inputObject;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            return addedObject;
+        }
+
+        public static List<T> InsertList<T>(List<T> inputList) where T : class
+        {
+            List<T> resultList = new List<T>();
+            try
+            {
+                using var db = new MoneyContext();
+                foreach (var item in inputList)
+                {
+                    var set = db.Set<T>();
+                    set.Add(item);
+                }
+
+                int saveResult = db.SaveChanges();
+                if (saveResult > 0)
+                    resultList = inputList;
+            }
+            catch (Exception ex) { }
+
+            return resultList;
+        }
+
+        public static List<T> GetAll<T>(Expression<Func<T, bool>> expression = null) where T : class, ICommon
+        {
+            List<T> returnList = new List<T>();
+            try
+            {
+                using var db = new MoneyContext();
+                //DbSet<T> dbSet;// = null;
+
+                DbSet<T> dbSet = db.Set<T>();
+
+                if (typeof(T) == typeof(MoneyItem))
+                {
+                    var ff = db.MoneyItems.Include(x => x.MainCategory);
+                    var ssdbSet = db.Set<MoneyItem>();
+                }
+                else
+                    dbSet = db.Set<T>();
+
+                if (expression != null)
+                {
+                    returnList = dbSet.Where(expression).ToList();
+                }
+                else
+                {
+                    returnList = dbSet.ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                //Insert(new Log() { Message = ex.Message, StackTrace = ex.StackTrace });
+            }
+            return returnList;
+        }
+
+        public static T Get<T>(Expression<Func<T, bool>> expression = null) where T : class
+        {
+            T returnObject;
+            try
+            {
+                using var db = new MoneyContext();
+                var dbSet = db.Set<T>();
+                if (expression != null)
+                {
+                    returnObject = dbSet.Where(expression).FirstOrDefault();
+                }
+                else
+                {
+                    returnObject = dbSet.FirstOrDefault();
+                }
+                return returnObject;
+            }
+            catch (Exception ex)
+            {
+                //Log.Report(ex);
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Object 개수 Counting, 조건 없으면 전체 개수 가져오기
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="expression"></param>
+        /// <returns></returns>
+        public static int Count<T>(Expression<Func<T, bool>> expression = null) where T : class
+        {
+            int count = 0;
+            try
+            {
+                using var db = new MoneyContext();
+                var dbSet = db.Set<T>();
+                if (expression != null)
+                    count = dbSet.Where(expression).ToList().Count;
+                else
+                    count = dbSet.ToList().Count;
+            }
+            catch (Exception ex)
+            {
+                throw;
+                //Insert(new Log() { Message = ex.Message, StackTrace = ex.StackTrace });
+            }
+            return count;
+        }
+
+        public static bool Delete<T>(T deleteObject) where T : class
+        {
+            bool result = false;
+            try
+            {
+                //if (deleteObject == null || deleteObject.Id == Guid.Empty)
+                //    return false;
+
+                using var db = new MoneyContext();
+                db.Entry(deleteObject).State = EntityState.Deleted;
+                var set = db.Set<T>();
+                set.Remove(deleteObject);
+                int saveResult = db.SaveChanges();
+                if (saveResult > 0)
+                    result = true;
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return result;
+        }
+
+        public static T Update<T>(T updateObject) where T : class
+        {
+            T addedObject = null;
+            try
+            {
+                using var db = new MoneyContext();
+                db.Entry(updateObject).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                var set = db.Set<T>();
+                set.Update(updateObject);
+                int saveResult = db.SaveChanges();
+                if (saveResult > 0)
+                    addedObject = updateObject;
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return addedObject;
+        }
+    }
+}
