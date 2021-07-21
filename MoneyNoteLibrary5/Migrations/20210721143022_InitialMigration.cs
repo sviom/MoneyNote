c02c92b86c06b0b8b4eb7bf458e73e1de4f7da72
강한별 <kanghanstar@outlook.com>
2021-07-21 23:34:53 +0900

    마이그레이션 초기화

---
+++ MoneyNoteLibrary5/Migrations/20210721143022_InitialMigration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MoneyNoteLibrary5.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
