27ab6c403e377f498e59dc0dee9870e18de60828
강한별 <kanghanstar@outlook.com>
2021-07-23 22:15:34 +0900

    FK 키 제거

---
+++ MoneyNoteLibrary5/Migrations/20210723130809_removeBankBookRequired2.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MoneyNoteLibrary5.Migrations
+{
+    public partial class removeBankBookRequired2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey("FK_MoneyItems_BankBooks_BankBookId", "MoneyItems");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
