632969575b50710b0049b0abbe4e164b004388b1
강한별 <kanghanstar@outlook.com>
2021-10-04 22:32:56 +0900

    허용 api 수정

--- MoneyNoteAPI/Startup.cs
+++ MoneyNoteAPI/Startup.cs
@@ -26,7 +26,7 @@ namespace MoneyNoteAPI
 
         public IConfiguration Configuration { get; }
 
-        public string[] corsDomains => new string[] { "https://localhost:44327", "http://moneynote.azurewebsites.net" };
+        public string[] corsDomains => new string[] { "https://localhost:44327", "https://moneynote.azurewebsites.net" };
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
