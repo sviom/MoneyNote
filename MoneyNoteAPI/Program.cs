19da6f0e81a37a9addcd0aff85368bc4fcee2d6d
HanByulKang <kanghanstar@gmail.com>
2022-09-09 14:47:06 +0900

    .NET 6 스타일 코드변경 및 의존성 및 기존 Startup.cs에서 사용되던 내용 추가

--- MoneyNoteAPI/Program.cs
+++ MoneyNoteAPI/Program.cs
@@ -7,80 +7,53 @@ using Azure.Identity;
 using Azure.Security.KeyVault.Secrets;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MoneyNoteAPI.Context;
 using MoneyNoteLibrary5.Common;
 
-namespace MoneyNoteAPI
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+builder.Services.AddMvc();
+builder.Services.AddControllers();
+builder.Services.AddCors(options =>
 {
+    options.AddDefaultPolicy(x => x.WithOrigins("https://localhost:44327", "https://moneynote.azurewebsites.net")
+        .AllowAnyHeader()
+        .AllowAnyMethod());
+});
 
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
+builder.Services.AddScoped<MoneyContext>();
+builder.Services.AddDbContext<MoneyContext>(options =>
+                    options.UseSqlServer(AzureKeyVault.OnGetAsync(KeyVaultName.MoneyNoteConnectionString).Result),
+                    ServiceLifetime.Scoped,
+                    ServiceLifetime.Scoped);
 
-            var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
 
-            // Add services to the container.
-            builder.Services.AddRazorPages();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.MapRazorPages();
-
-            app.Run();
-
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-            .ConfigureAppConfiguration((context, config) =>
-            {
-                //if (context.HostingEnvironment.IsProduction())
-                //{
-                //}
-
-                //config.AddAzureAppConfiguration(options =>
-                //{
-                //    options.Connect(settings["ConnectionStrings:AppConfig"])
-                //            .ConfigureKeyVault(kv =>
-                //            {
-                //                var cert = new ClientSecretCredential("<tenant id>", "client id", "client secret");
-                //                kv.SetCredential(cert);
-                //            });
-                //});
-
-                //var keyVaultEndpoint = AzureKeyVault.GetKeyVaultEndpoint();
-                //if (!string.IsNullOrEmpty(keyVaultEndpoint))
-                //{
-                //    var azureServiceTokenProvider = new AzureServiceTokenProvider();
-                //    var keyVaultClient = new KeyVaultClient(
-                //        new KeyVaultClient.AuthenticationCallback(
-                //            azureServiceTokenProvider.KeyVaultTokenCallback));
-                //    config.AddAzureKeyVault(keyVaultEndpoint, keyVaultClient, new DefaultKeyVaultSecretManager());
-                //}
-            })
-
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
-    }
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseCors();
+
+app.MapRazorPages();
+
+app.Run();
