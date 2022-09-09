492232091d09ae72593fa844554bb0b72168a249
HanByulKang <kanghanstar@gmail.com>
2022-09-09 15:23:31 +0900

    Blazor 시작 추가

--- MoneyNoteBlazor/Program.cs
+++ MoneyNoteBlazor/Program.cs
@@ -1,8 +1,10 @@
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.JSInterop;
+using MoneyNoteBlazor;
 using MoneyNoteLibrary5.Models;
 using Newtonsoft.Json;
 using System;
@@ -11,26 +13,16 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
+
 namespace MoneyNoteBlazor
 {
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
-
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-            if (builder.HostEnvironment.IsDevelopment())
-            {
-
-            }
-
-            await builder.Build().RunAsync();
-        }
-    }
-
     public static class SharedClass
     {
         public static User SharedUser { get; set; }
