e7e6e7890fc24df52a6ee748eb84285caa8dac60
HanByulKang <kanghanstar@gmail.com>
2022-09-17 20:49:38 +0900

    Auth관련 코드를 미들웨어로 이관 준비

---
+++ MoneyNoteAPI/AuthMiddleware.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace MoneyNoteAPI
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class AuthMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public AuthMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext httpContext)
+        {
+
+            return _next(httpContext);
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class AuthMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseMiddlewareClassTemplate(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<AuthMiddleware>();
+        }
+    }
+}
+
