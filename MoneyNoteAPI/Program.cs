c6e8708ed42c9f95c75b2f16de31da6e335ae5b3
HanByulKang <kanghanstar@gmail.com>
2022-09-17 20:31:26 +0900

    Token을 위한 서비스 생서

--- MoneyNoteAPI/Program.cs
+++ MoneyNoteAPI/Program.cs
@@ -1,16 +1,19 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Azure.Extensions.AspNetCore.Configuration.Secrets;
 using Azure.Identity;
 using Azure.Security.KeyVault.Secrets;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using MoneyNoteAPI.Context;
 using MoneyNoteLibrary5.Common;
 
@@ -34,6 +37,27 @@ builder.Services.AddDbContext<MoneyContext>(options =>
                     ServiceLifetime.Scoped,
                     ServiceLifetime.Scoped);
 
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(o =>
+{
+    o.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = false,
+        ValidateIssuerSigningKey = true
+    };
+});
+
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -52,10 +76,12 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+app.UseAuthentication();
+
 app.UseCors();
 
 app.MapRazorPages();
 
-app.MapControllers();
+app.MapControllers().RequireAuthorization();
 
 app.Run();
---
+++ MoneyNoteAPI/Services/TokenService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.IdentityModel.Tokens;
+using MoneyNoteLibrary5.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace MoneyNoteAPI.Services
+{
+    public class TokenService
+    {
+        public TokenService()
+        {
+
+        }
+
+        public IResult CreateToken(User user)
+        {
+            if (user.Name == "joydip" && user.Password == "joydip123")
+            {
+                // appSetting.json에서 값 가져오는 방법 찾기
+                var issuer = builder.Configuration["Jwt:Issuer"];
+                var audience = builder.Configuration["Jwt:Audience"];
+                var key = Encoding.ASCII.GetBytes
+                (builder.Configuration["Jwt:Key"]);
+                var tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(new[]
+                    {
+                new Claim("Id", Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Sub, user.Name),
+                new Claim(JwtRegisteredClaimNames.Email, user.Name),
+                new Claim(JwtRegisteredClaimNames.Jti,
+                Guid.NewGuid().ToString())
+             }),
+                    Expires = DateTime.UtcNow.AddMinutes(5),
+                    Issuer = issuer,
+                    Audience = audience,
+                    SigningCredentials = new SigningCredentials
+                    (new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha512Signature)
+                };
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var token = tokenHandler.CreateToken(tokenDescriptor);
+                var jwtToken = tokenHandler.WriteToken(token);
+                var stringToken = tokenHandler.WriteToken(token);
+                return Results.Ok(stringToken);
+            }
+            return Results.Unauthorized();
+        }
+    }
+}
+
