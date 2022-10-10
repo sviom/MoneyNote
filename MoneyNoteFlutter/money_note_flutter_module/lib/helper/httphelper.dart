6ebc06ed32120ddd377ec5d60b0ccab6c892b380
HanByulKang <kanghanstar@gmail.com>
2022-10-10 14:48:01 +0900

    Helper에 샘플 추가

--- MoneyNoteFlutter/money_note_flutter_module/lib/helper/httphelper.dart
+++ MoneyNoteFlutter/money_note_flutter_module/lib/helper/httphelper.dart
@@ -1,5 +1,13 @@
-
+import 'package:http/http.dart' as http;
 
 class httpHeler {
+  Post() async {
+    var url = Uri.https('example.com', 'whatsit/create');
+    var response =
+        await http.post(url, body: {'name': 'doodle', 'color': 'blue'});
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
 
+    print(await http.read(Uri.https('example.com', 'foobar.txt')));
+  }
 }
