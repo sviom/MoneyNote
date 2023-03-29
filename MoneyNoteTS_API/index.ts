aa38556dcf5fa76fad3ad767244758be319376d9
강한별 <kanghanstar@outlook.com>
2023-03-29 22:29:13 +0900

    기본 설정 계속 추가

--- MoneyNoteTS_API/index.ts
+++ MoneyNoteTS_API/index.ts
@@ -1,2 +1,13 @@
-import { } from 'express';
+import express, { Response, Request } from 'express';
 
+const app = express();
+const port = 8080;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('test');
+})
+
+
+app.listen(port, () => {
+    console.log('start server');
+});
