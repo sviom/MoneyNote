e218328f2f1d542a958a31087265ceb6c796796d
강한별 <kanghanstar@outlook.com>
2023-03-29 22:18:26 +0900

    설정파일 관련 추가

--- MoneyNoteTS_API/.eslintrc.js
+++ MoneyNoteTS_API/.eslintrc.js
@@ -1,3 +1,23 @@
 module.exports = {
-
+    root: true,
+    env: {
+        browser: true,
+        node: true,
+        es6: true,
+    },
+    parserOptions: {
+        sourceType: 'module',
+        parser: '@babel/eslint-parser',
+        ecmaVersion: 2022,
+    },
+    extends: [
+        // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+        // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+        // 'plugin:vue/essential',
+        'eslint:recommended',
+    ],
+    // required to lint *.vue files
+    // plugins: ['vue'],
+    // add your custom rules here
+    rules: {},
 };
