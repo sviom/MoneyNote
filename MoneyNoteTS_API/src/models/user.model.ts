b106167e7074fb4314ab4a29ecf0fdc4fbfdea1d
강한별 <kanghanstar@gmail.com>
2023-04-13 21:39:04 +0900

    TEST

--- MoneyNoteTS_API/src/models/user.model.ts
+++ MoneyNoteTS_API/src/models/user.model.ts
@@ -3,6 +3,12 @@ import { ICommon } from './common.model';
 class User implements ICommon {
     public guid: string;
 
+    public name: string;
+
+    public email: string;
+
+    public password: string;
+
     constructor() {
         this.guid = '';
     }
